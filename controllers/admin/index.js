4bd7e76e0aa3d985e07f1ab2bdfdcedb2fc56ff1
임찬엽 <limchanyeop@imchan-yeob-ui-MacBookPro.local>
2021-09-14 10:46:44 +0900

    test frontend link

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -4,6 +4,7 @@ const ctrl = require('./admin.ctrl');
 const upload = require('../../middleware/multer');
 
 router.post('/name', upload.single('photo'), ctrl.getName);
-router.get('/name/:name/:lat/:lng', ctrl.showMap)
+// router.get('/name/:name/:lat/:lng', ctrl.showMap)
+router.get('/name', ctrl.showMap)
 // router.post('/name', ctrl.get_name);
 module.exports = router;
