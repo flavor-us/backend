4e725ad3093fba33bb26d9e8999313bc0882f925
임찬엽 <limchanyeop@imchan-yeob-ui-MacBookPro.local>
2021-09-17 00:08:14 +0900

    adminctrl array debug, handling Exception

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -2,13 +2,13 @@ const db = require('../../models');
 var ExifImage = require('exif').ExifImage;
 
 exports.getName = async ( req, res ) => {
-    console.log("Post")
+    console.log("들어옴");
     let moe = 0.00001;
     var name;
     const selectNameQuery = `SELECT name FROM LocationDB
                 WHERE 
-                    lat BETWEEN ? AND ? and
-                    lng BETWEEN ? AND ?`
+                    lat >= ? and lat <= ? and 
+                    lng >= ? and lng <= ?`
     function getExif(file) {
         return new Promise(function (resolve, reject) {
             try {
@@ -59,29 +59,43 @@ exports.getName = async ( req, res ) => {
     });
     if (gpsDMS)
     {
+        console.log("들어옴2")
         console.log(gpsDMS)
         const gpsDegree = convertLatLng(gpsDMS[0], gpsDMS[1]);
         console.log(gpsDegree);
+        console.log("들어옴3")
         if (gpsDegree)
         { 
+            console.log("들어옴4")
             do {
                 name = await getName(selectNameQuery, gpsDegree[0], gpsDegree[1]);
                 console.log(moe);
-                moe *= 2;
+                moe *= 2
                 if (moe > 0.01)
                     break ;
             } while ( Object.keys(name).length < 3 )
-            console.log(name);
+
+arrayForDebug = name.map((item) => {
+return item.name
+})
+
             console.log(gpsDegree)
-            // res.render('admin/select.html', { name , gpsDegree }); // web
-            res.send({ name : name }); //  android
+            // res.render('admin/select.html', { name , gpsDegree });
+            res.send({ name : arrayForDebug});
+        } else {
+            res.send( {
+                name: [ "Not Found" ],
+                msg: "EXIF 데이터가 있으나, 위도 경도 정보는 찾을 수 없었습니다."
+            } )
         }
     }
     else
-        res.send("Error : Can't get ExifData")
+         res.send( {
+                msg: "Cannot get ExifData"
+         } )
 }
 
 exports.showMap =  async (req, res) => {
     console.log(req.params);
-    res.render('admin/name.html', { name : req.query.name , lat : req.query.lat , lng : req.query.lng });
+    res.render('admin/name.html', { name : req.params.name , lat : req.params.lat , lng : req.params.lng });
 }
