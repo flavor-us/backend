ff6b610820022fd418041d256f0d8c45612252d4
임찬엽 <153893@naver.com>
2021-10-10 23:48:58 +0900

    disable sync

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ class App {
 				.authenticate()
 				.then(() => {
 					console.log("Connection has been established successfully.");
-					return db.sequelize.sync();
+					// return db.sequelize.sync();
 				})
 				.then(() => {
 					console.log("DB Sync complete.");
