1c4830f0c6b9eeefb5fc0a187fabf51eac3e2470
임찬엽 <153893@naver.com>
2021-10-17 22:53:34 +0900

    test admin page search function

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,8 +1,19 @@
 const models = require("../../models");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 exports.getContents = async (req, res) => {
 	try {
-		const [contents] = await Promise.all([models.Contents.findAll()]);
+		const [contents] = await Promise.all([models.Contents.findAll({
+			where: {
+				...('name' in req.query && req.query.name ? {
+					filename: {
+						[Op.like]: "%" + req.query.name + "%"
+					}
+				} : '')
+			}
+		}
+		)]);
 		res.render("admin/contents.html", { contents });
 	} catch (e) {
 		console.log(e);
