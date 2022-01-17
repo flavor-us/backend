f1cb04264fe38fd442ab577c8f5ec3bf295c8968
chanyeop <153893@naver.com>
2022-01-17 21:49:22 +0900

    add privacy policy page

--- controllers/web/index.js
+++ controllers/web/index.js
@@ -5,5 +5,5 @@ const upload = require("../../middleware/multer");
 
 router.post("/name", upload.single("photo"), ctrl.getNames);
 router.post("/upload", ctrl.dbUpload);
-
+router.get("/privacypolicy", async (req, res) => { return (res.render("web/policy.html")); })
 module.exports = router;
