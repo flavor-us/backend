bdb30a5979ff15de5afc6b2b4dfb07b263324c77
chanyeop <153893@naver.com>
2022-07-02 00:20:01 +0900

    Feat: getRelevant api comment contain kakao_id

--- controllers/app/contents/contents.ctrl.js
+++ controllers/app/contents/contents.ctrl.js
@@ -10,166 +10,180 @@ const Op = Sequelize.Op;
 const logger = require("../../../config/logger");
 
 exports.uploadContents = async (req, res) => {
-    var content_id, city;
-    try {
-        if (!req.body.lat || !req.body.lng || !req.body.filename || !req.body.restname)
-            throw (errorMsg.notEnoughReq);
-        const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.body.kakao_id);
-        const station = await nearStation.getNearStation(req.body.lat, req.body.lng);
-        if (!station)
-            city = await nearCity.getNearCity(req.body.lat, req.body.lng);
-        const content = {
-            user_id: user_id,
-            rest_id: req.body.rest_id,
-            date: new Date(),
-            filename: req.body.filename,
-            filepath: process.env.BUCKET_ADDR + "/" + user_id + "/" + req.body.filename,
-            rekognition: req.body.rekog,
-            restname: req.body.restname,
-            lat: req.body.lat,
-            lng: req.body.lng,
-            adj1_id: req.body.adj1_id,
-            adj2_id: req.body.adj2_id,
-            locationtag_id: req.body.locationtag_id,
-            near_station: station ? station.name : city.name,
-            near_point: station ? station.name : city.name,
-            station_distance: station ? station.distance : city.distance,
-            point_distance: station ? station.distance : city.distance
-        }
-        content_id = await dbUpload.uploadContent(content);
-    } catch (e) {
-        console.log(e);
-        logger.error("[uploadContents] : " + e);
-        if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        if (e == errorMsg.noUser)
-            return (res.status(400).send(errorMsg.noUser));
-        else
-            return (res.status(400).send(errorMsg.uploadFail));
-    };
-    return (res.status(201).send({ msg: completeMsg.uploadComplete.msg, content_id: content_id }));
+	var content_id, city;
+	try {
+		if (!req.body.lat || !req.body.lng || !req.body.filename || !req.body.restname)
+			throw (errorMsg.notEnoughReq);
+		const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.body.kakao_id);
+		const station = await nearStation.getNearStation(req.body.lat, req.body.lng);
+		if (!station)
+			city = await nearCity.getNearCity(req.body.lat, req.body.lng);
+		const content = {
+			user_id: user_id,
+			rest_id: req.body.rest_id,
+			date: new Date(),
+			filename: req.body.filename,
+			filepath: process.env.BUCKET_ADDR + "/" + user_id + "/" + req.body.filename,
+			rekognition: req.body.rekog,
+			restname: req.body.restname,
+			lat: req.body.lat,
+			lng: req.body.lng,
+			adj1_id: req.body.adj1_id,
+			adj2_id: req.body.adj2_id,
+			locationtag_id: req.body.locationtag_id,
+			near_station: station ? station.name : city.name,
+			near_point: station ? station.name : city.name,
+			station_distance: station ? station.distance : city.distance,
+			point_distance: station ? station.distance : city.distance
+		}
+		content_id = await dbUpload.uploadContent(content);
+	} catch (e) {
+		console.log(e);
+		logger.error("[uploadContents] : " + e);
+		if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		if (e == errorMsg.noUser)
+			return (res.status(400).send(errorMsg.noUser));
+		else
+			return (res.status(400).send(errorMsg.uploadFail));
+	};
+	return (res.status(201).send({ msg: completeMsg.uploadComplete.msg, content_id: content_id }));
 }
 
 exports.updateContents = async (req, res) => {
-    var content_id;
-    try {
-        let content = {};
-        if ((!req.body.adj1_id && !req.body.adj2_id && !req.body.locationtag_id) || (!req.params.content_id))
-            throw (errorMsg.notEnoughReq)
-        if (req.body.adj1_id)
-            content.adj1_id = req.body.adj1_id
-        if (req.body.adj2_id)
-            content.adj2_id = req.body.adj2_id
-        if (req.body.locationtag_id)
-            content.locationtag_id = req.body.locationtag_id
-        content_id = await dbUpload.updateContents(content, req.params.content_id);
-    } catch (e) {
-        logger.error("[updateContents] : " + JSON.stringify(e));
-        if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        else
-            return (res.status(400).send(errorMsg.updateFail));
-    }
-    return (res.status(201).send({ msg: completeMsg.updateComplete.msg, content_id: content_id }));
+	var content_id;
+	try {
+		let content = {};
+		if ((!req.body.adj1_id && !req.body.adj2_id && !req.body.locationtag_id) || (!req.params.content_id))
+			throw (errorMsg.notEnoughReq)
+		if (req.body.adj1_id)
+			content.adj1_id = req.body.adj1_id
+		if (req.body.adj2_id)
+			content.adj2_id = req.body.adj2_id
+		if (req.body.locationtag_id)
+			content.locationtag_id = req.body.locationtag_id
+		content_id = await dbUpload.updateContents(content, req.params.content_id);
+	} catch (e) {
+		logger.error("[updateContents] : " + JSON.stringify(e));
+		if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		else
+			return (res.status(400).send(errorMsg.updateFail));
+	}
+	return (res.status(201).send({ msg: completeMsg.updateComplete.msg, content_id: content_id }));
 }
 
 exports.deleteContents = async (req, res) => {
-    logger.info(`${req.method} ${req.url}`);
-    try {
-        if (!req.params.content_id)
-            throw (errorMsg.notEnoughReq);
-        const content = await models.Contents.destroy({
-            where: {
-                id: req.params.content_id
-            }
-        });
-        if (!content)
-            throw (errorMsg.noContent);
-    } catch (e) {
-        logger.error("[deleteContents] : " + JSON.stringify(e));
-        if (e == errorMsg.noContent)
-            return (res.status(400).send(errorMsg.noContent));
-        else if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        else
-            return (res.status(400).send(errorMsg.deleteFail));
-    }
-    return (res.status(204).send());
+	logger.info(`${req.method} ${req.url}`);
+	try {
+		if (!req.params.content_id)
+			throw (errorMsg.notEnoughReq);
+		const content = await models.Contents.destroy({
+			where: {
+				id: req.params.content_id
+			}
+		});
+		if (!content)
+			throw (errorMsg.noContent);
+	} catch (e) {
+		logger.error("[deleteContents] : " + JSON.stringify(e));
+		if (e == errorMsg.noContent)
+			return (res.status(400).send(errorMsg.noContent));
+		else if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		else
+			return (res.status(400).send(errorMsg.deleteFail));
+	}
+	return (res.status(204).send());
 }
 
 exports.getMyContents = async (req, res) => {
-    var contents;
-    logger.info(`${req.method} ${req.url}`);
-    try {
-        if (!req.params.kakao_id)
-            throw (errorMsg.notEnoughReq);
-        const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.params.kakao_id);
-        contents = await models.Contents.findAll({
-            include: [
-                { attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
-                { attributes: ["tagname"], model: models.Tag_FirstAdj },
-                { attributes: ["tagname"], model: models.Tag_SecondAdj },
-                { attributes: ["tagname"], model: models.Tag_Location },
-                { attributes: ["id", "user_id", "content"], model: models.Comments }
-            ],
-            where: { user_id: user_id }
-        })
-    } catch (e) {
-        logger.error("[getMyContents] : " + JSON.stringify(e));
-        if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        else if (e == errorMsg.noUser)
-            return (res.status(400).send(errorMsg.noUser));
-        else
-            return (res.status(400).send(errorMsg.readFail));
-    }
-    return (res.status(200).send({ result: contents }));
+	var contents;
+	logger.info(`${req.method} ${req.url}`);
+	try {
+		if (!req.params.kakao_id)
+			throw (errorMsg.notEnoughReq);
+		const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.params.kakao_id);
+		contents = await models.Contents.findAll({
+			include: [
+				{ attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
+				{ attributes: ["tagname"], model: models.Tag_FirstAdj },
+				{ attributes: ["tagname"], model: models.Tag_SecondAdj },
+				{ attributes: ["tagname"], model: models.Tag_Location },
+				{ attributes: ["id", "user_id", "content"], model: models.Comments }
+			],
+			where: { user_id: user_id }
+		})
+	} catch (e) {
+		logger.error("[getMyContents] : " + JSON.stringify(e));
+		if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		else if (e == errorMsg.noUser)
+			return (res.status(400).send(errorMsg.noUser));
+		else
+			return (res.status(400).send(errorMsg.readFail));
+	}
+	return (res.status(200).send({ result: contents }));
 }
 
 exports.getRelevantContents = async (req, res) => {
-    logger.info(`${req.method} ${req.url}`);
-    var contents;
-    try {
-        if (!req.params.kakao_id)
-            throw (errorMsg.notEnoughReq);
-        const maxCnt = (req.query.max && req.query.max < 50 && req.query.max > 0) ? req.query.max : null;
-        const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.params.kakao_id);
-        const friends = await models.Relation.findAll({
-            attributes: ['followed_id'],
-            where: {
-                follower_id: user_id
-            }
-        })
-        const friendList = friends.map((item) => {
-            return item.dataValues.followed_id;
-        })
-        contents = await models.Contents.findAll({
-            include: [
-                { attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
-                { attributes: ["tagname"], model: models.Tag_FirstAdj },
-                { attributes: ["tagname"], model: models.Tag_SecondAdj },
-                { attributes: ["tagname"], model: models.Tag_Location },
-                { attributes: ["id", "user_id", "content"], model: models.Comments }
-            ],
-            where: { [Op.or]: [{ user_id: { [Op.in]: friendList } }, { user_id: user_id }] },
-            order: [['date', 'DESC']],
-            limit: maxCnt ? Number(maxCnt) : null
-        })
-        contents = contents.map((content) => {
-            if (content.dataValues.near_station && content.dataValues.station_distance) {
-                content.dataValues.near_point = content.dataValues.near_station;
-                content.dataValues.point_distance = content.dataValues.station_distance;
-            }
-            return (content);
-        });
-    } catch (e) {
-        logger.error("[getRelevantContents] : " + JSON.stringify(e));
-        if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        else if (e == errorMsg.noUser)
-            return (res.status(400).send(errorMsg.noUser));
-        else
-            return (res.status(400).send(errorMsg.readFail));
-    }
-    return (res.status(200).send({ result: contents }));
+	logger.info(`${req.method} ${req.url}`);
+	var contents;
+	try {
+		if (!req.params.kakao_id)
+			throw (errorMsg.notEnoughReq);
+		const maxCnt = (req.query.max && req.query.max < 50 && req.query.max > 0) ? req.query.max : null;
+		const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.params.kakao_id);
+		const friends = await models.Relation.findAll({
+			attributes: ['followed_id'],
+			where: {
+				follower_id: user_id
+			}
+		})
+		const friendList = friends.map((item) => {
+			return item.dataValues.followed_id;
+		})
+		contents = await models.Contents.findAll({
+			include: [
+				{ attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
+				{ attributes: ["tagname"], model: models.Tag_FirstAdj },
+				{ attributes: ["tagname"], model: models.Tag_SecondAdj },
+				{ attributes: ["tagname"], model: models.Tag_Location },
+				{ attributes: ["id", "kakao_id", "content"], model: models.Comments },
+			],
+			where: { [Op.or]: [{ user_id: { [Op.in]: friendList } }, { user_id: user_id }] },
+			order: [['date', 'DESC']],
+			limit: maxCnt ? Number(maxCnt) : null
+		})
+
+		contents = contents.map((content) => {
+			if (content.dataValues.near_station && content.dataValues.station_distance) {
+				content.dataValues.near_point = content.dataValues.near_station;
+				content.dataValues.point_distance = content.dataValues.station_distance;
+			}
+			return (content);
+		});
+		// contents = await Promise.all(contents.map(async (content) => {
+		// 	if (content.dataValues.near_station && content.dataValues.station_distance) {
+		// 		content.dataValues.near_point = content.dataValues.near_station;
+		// 		content.dataValues.point_distance = content.dataValues.station_distance;
+		// 	}
+		// 	content.Comments = content.Comments.map(async (comment) => {
+		// 		// console.log("BEFORE: " + JSON.stringify(comment));
+		// 		comment.dataValues.kakao_id = await kakaoIdConvert.getKakaoIdByUserId(comment.dataValues.user_id);
+		// 		// console.log("AFTER: " + JSON.stringify(comment));
+		// 		return (JSON.stringify(comment));
+		// 	})
+		// 	return (content);
+		// }));
+	} catch (e) {
+		logger.error("[getRelevantContents] : " + JSON.stringify(e));
+		if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		else if (e == errorMsg.noUser)
+			return (res.status(400).send(errorMsg.noUser));
+		else
+			return (res.status(400).send(errorMsg.readFail));
+	}
+	return (res.status(200).send({ result: contents }));
 }
