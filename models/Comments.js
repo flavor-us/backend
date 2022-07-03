7a66f063213ac6bb6a250b53da638be0831d1039
chanyeop <153893@naver.com>
2022-07-03 12:30:59 +0900

    Feat: change db Model

--- models/Comments.js
+++ models/Comments.js
@@ -1,35 +1,40 @@
 module.exports = function (sequelize, DataTypes) {
-    const Comments = sequelize.define(
-        "Comments",
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            content: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-        },
-        {
-            sequelize,
-            tableName: "Comments",
-            timestamps: true,
-        },
-    );
-    Comments.associate = (models) => {
-        Comments.belongsTo(models.User, {
-            foreignKey: "user_id",
-            targetKey: "id",
-            unique: false,
-        })
-        Comments.belongsTo(models.Contents, {
-            foreignKey: "content_id",
-            targetKey: "id",
-            unique: false,
-        })
-    }
-    return Comments;
+	const Comments = sequelize.define(
+		"Comments",
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			content: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+		},
+		{
+			sequelize,
+			tableName: "Comments",
+			timestamps: true,
+		},
+	);
+	Comments.associate = (models) => {
+		Comments.belongsTo(models.User, {
+			foreignKey: "user_id",
+			targetKey: "id",
+			unique: false,
+		})
+		Comments.belongsTo(models.Contents, {
+			foreignKey: "content_id",
+			targetKey: "id",
+			unique: false,
+		}),
+			Comments.belongsTo(models.User, {
+				foreignKey: "kakao_id",
+				targetKey: "kakao_id",
+				unique: false,
+			})
+	}
+	return Comments;
 };
