edd15faee0716a36da40a7d3641e1d6c4cf90e01
chanyeop <153893@naver.com>
2022-07-05 22:00:41 +0900

    Fix: upload comment with kakao_id

--- controllers/app/comments/comments.ctrl.js
+++ controllers/app/comments/comments.ctrl.js
@@ -6,51 +6,52 @@ const dbUpload = require("../../../modules/dbUpload");
 const logger = require("../../../config/logger");
 
 exports.uploadComments = async (req, res) => {
-    var comment_id;
-    logger.info(`${req.method} ${req.url}`);
-    if (!req.body.kakao_id || !req.body.content)
-        return (res.status(400).send(errorMsg.notEnoughReq));
-    try {
-        const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.body.kakao_id);
-        const comment = {
-            user_id: user_id,
-            content_id: req.params.content_id,
-            content: req.body.content
-        }
-        comment_id = await dbUpload.uploadComment(comment);
-    } catch (e) {
-        logger.error("[uploadComments] : " + JSON.stringify(e));
-        if (e == errorMsg.noUser)
-            return (res.status(400).send(errorMsg.noUser));
-        else if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        else
-            return (res.status(400).send(errorMsg.uploadFail));
-    }
-    return (res.status(201).send({ msg: completeMsg.uploadComplete.msg, comment_id: comment_id }));
+	var comment_id;
+	logger.info(`${req.method} ${req.url}`);
+	if (!req.body.kakao_id || !req.body.content || req.params.content_id)
+		return (res.status(400).send(errorMsg.notEnoughReq));
+	try {
+		const user_id = await kakaoIdConvert.getUserIdByKakaoId(req.body.kakao_id);
+		const comment = {
+			user_id: user_id,
+			content_id: req.params.content_id,
+			content: req.body.content,
+			kakao_id: req.body.kakao_id
+		}
+		comment_id = await dbUpload.uploadComment(comment);
+	} catch (e) {
+		logger.error("[uploadComments] : " + JSON.stringify(e));
+		if (e == errorMsg.noUser)
+			return (res.status(400).send(errorMsg.noUser));
+		else if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		else
+			return (res.status(400).send(errorMsg.uploadFail));
+	}
+	return (res.status(201).send({ msg: completeMsg.uploadComplete.msg, comment_id: comment_id }));
 }
 
 exports.deleteComments = async (req, res) => {
-    logger.info(`${req.method} ${req.url}`);
-    try {
-        if (!req.params.comment_id)
-            throw (errorMsg.notEnoughReq);
-        const comment = await models.Comments.destroy({
-            where: {
-                id: req.params.comment_id
-            }
-        });
-        if (!comment)
-            throw (errorMsg.noComment);
-    } catch (e) {
-        logger.error("[deleteComments] : " + JSON.stringify(e));
-        if (e == errorMsg.notEnoughReq)
-            return (res.status(400).send(errorMsg.notEnoughReq));
-        else if (e == errorMsg.noComment)
-            return (res.status(400).send(errorMsg.noComment));
-        else
-            return (res.status(400).send(errorMsg.deleteFail));
-    }
-    return (res.status(204).send());
+	logger.info(`${req.method} ${req.url}`);
+	try {
+		if (!req.params.comment_id)
+			throw (errorMsg.notEnoughReq);
+		const comment = await models.Comments.destroy({
+			where: {
+				id: req.params.comment_id
+			}
+		});
+		if (!comment)
+			throw (errorMsg.noComment);
+	} catch (e) {
+		logger.error("[deleteComments] : " + JSON.stringify(e));
+		if (e == errorMsg.notEnoughReq)
+			return (res.status(400).send(errorMsg.notEnoughReq));
+		else if (e == errorMsg.noComment)
+			return (res.status(400).send(errorMsg.noComment));
+		else
+			return (res.status(400).send(errorMsg.deleteFail));
+	}
+	return (res.status(204).send());
 
 }
