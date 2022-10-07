71c09e0f8318eeb7f65df2bdbbc1a0b43799cc3f
chanyeop <153893@naver.com>
2022-10-07 22:42:50 +0900

    Feat: pagenation of GetRelevantContents

--- controllers/app/contents/contents.ctrl.js
+++ controllers/app/contents/contents.ctrl.js
@@ -129,6 +129,11 @@ exports.getMyContents = async (req, res) => {
 exports.getRelevantContents = async (req, res) => {
 	logger.info(`${req.method} ${req.url}`);
 	var contents;
+	if (req.query.page && req.query.count) {
+		var page = Number(req.query.page);
+		var count = Number(req.query.count);
+		var offset = Number(count * (page - 1));
+	}
 	try {
 		if (!req.params.kakao_id)
 			throw (errorMsg.notEnoughReq);
@@ -143,40 +148,56 @@ exports.getRelevantContents = async (req, res) => {
 		const friendList = friends.map((item) => {
 			return item.dataValues.followed_id;
 		})
-		contents = await models.Contents.findAll({
-			include: [
-				{ attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
-				{ attributes: ["tagname"], model: models.Tag_FirstAdj },
-				{ attributes: ["tagname"], model: models.Tag_SecondAdj },
-				{ attributes: ["tagname"], model: models.Tag_Location },
-				{ attributes: ["id", "user_id", "content", "createdAt"], model: models.Comments },
-			],
-			where: { [Op.or]: [{ user_id: { [Op.in]: friendList } }, { user_id: user_id }] },
-			order: [['date', 'DESC']],
-			limit: maxCnt ? Number(maxCnt) : null
-		})
+		if (!req.query.page || !req.query.count) {
+			contents = await models.Contents.findAll({
+				include: [
+					{ attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
+					{ attributes: ["tagname"], model: models.Tag_FirstAdj },
+					{ attributes: ["tagname"], model: models.Tag_SecondAdj },
+					{ attributes: ["tagname"], model: models.Tag_Location },
+					{ attributes: ["id", "user_id", "content", "createdAt"], model: models.Comments },
+				],
+				where: { [Op.or]: [{ user_id: { [Op.in]: friendList } }, { user_id: user_id }] },
+				order: [['date', 'DESC']],
+				limit: maxCnt ? Number(maxCnt) : null
+			})
+		} else {
+			contents = await models.Contents.findAll({
+				include: [
+					{ attributes: ["username", "profileimg_path", "kakao_id"], model: models.User },
+					{ attributes: ["tagname"], model: models.Tag_FirstAdj },
+					{ attributes: ["tagname"], model: models.Tag_SecondAdj },
+					{ attributes: ["tagname"], model: models.Tag_Location },
+					{ attributes: ["id", "user_id", "content", "createdAt"], model: models.Comments },
+				],
+				where: { [Op.or]: [{ user_id: { [Op.in]: friendList } }, { user_id: user_id }] },
+				order: [['date', 'DESC']],
+				offset: offset,
+				limit: count
+			})
 
-		contents = contents.map((content) => {
-			if (content.dataValues.near_station && content.dataValues.station_distance) {
-				content.dataValues.near_point = content.dataValues.near_station;
-				content.dataValues.point_distance = content.dataValues.station_distance;
-			}
-			return (content);
-		});
-		var i = -1, j = -1;
-		while (contents && contents[++i]) {
-			j = -1;
-			while (contents[i].Comments && contents[i].Comments[++j]) {
-				console.log(contents[i].Comments && contents[i].Comments[j]);
+			contents = contents.map((content) => {
+				if (content.dataValues.near_station && content.dataValues.station_distance) {
+					content.dataValues.near_point = content.dataValues.near_station;
+					content.dataValues.point_distance = content.dataValues.station_distance;
+				}
+				return (content);
+			});
+			var i = -1, j = -1;
+			while (contents && contents[++i]) {
+				j = -1;
+				while (contents[i].Comments && contents[i].Comments[++j]) {
+					console.log(contents[i].Comments && contents[i].Comments[j]);
 
-				var writer = await models.User.findOne({
-					where: {
-						id: contents[i].Comments[j].dataValues.user_id
-					}
-				})
-				contents[i].Comments[j].dataValues.username = writer.dataValues.username;
-				contents[i].Comments[j].dataValues.profileimg_path = writer.dataValues.profileimg_path;
-				contents[i].Comments[j].dataValues.kakao_id = writer.dataValues.kakao_id;
+					var writer = await models.User.findOne({
+						where: {
+							id: contents[i].Comments[j].dataValues.user_id
+						}
+					})
+					contents[i].Comments[j].dataValues.username = writer.dataValues.username;
+					contents[i].Comments[j].dataValues.profileimg_path = writer.dataValues.profileimg_path;
+					contents[i].Comments[j].dataValues.kakao_id = writer.dataValues.kakao_id;
+				}
 			}
 		}
 	} catch (e) {
